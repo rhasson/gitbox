0b34b888ccfe26cf6200626de9b20a2633823318
Roy Hasson <rhasson@gmail.com>
2013-10-08 15:27:34 -0400

    added staging area with subdirectory parsing

--- box.js
+++ box.js
@@ -5,12 +5,16 @@ var platform = require('git-node-platform')
   , nfs = require('graceful-fs')
   , watchr = require('watchr')
   , path = require('path')
+  , trie = require('path-trie')
   , config = require('./config').config;
+
+var util = require('util');
 
 // Create a filesystem backed bare repo
 var repo = jsGit(fsDb(fs("home.git")))
   , home = config.home.path //process.argv.splice(2)[0]
-  , index = {}
+  , index
+  , __parent = null
   , commiter = config.commiter
   , message = 'Commit message';
 
@@ -25,31 +29,19 @@ watchr.watch({
 			repo.loadAs('commit', 'HEAD', function(err, commit, hash) {
 				if (!err && commit && hash) {
 					repo.updateHead(hash, moveHead);
-					index['__parent'] = hash;
+					__parent = hash;
 				} else {
 					repo.setHead("master", function (err) {
 						if (err) throw err;
 						console.log('set HEAD to master');
-						index['__parent'] = null;
+						__parent = null;
 					});
 				} 
 			});
 		},
  		log: function(logLevel){
             var args = Array.prototype.slice.call(arguments);
-            var msg, src, dst;
-
-console.log(args)
-            args.shift(); //remove 'debug' as the first field
-            msg = args.shift();
-/*
-            if (/determined delete/i.test(msg)) {
-            	src = args.shift();
-            	handleDelete(src);
-            } else if (/determined /i.test(msg)) {
-
-            }
-*/
+            //console.log(args);
         },
 		change: function(changeType, filePath, currentStat, previousStat) {
 			switch (changeType) {
@@ -78,34 +70,65 @@ console.log(args)
 function handleCreate(filePath, currentStat, previousStat) {
 	console.log('handleCreate: ', filePath);
 	
-	if (currentStat.isDirectory()) {
-		//if (!(base in index)) index[base] = {};
-	} else if (currentStat.isFile()) {
-		saveBlob(filePath, currentStat, previousStat);
-	} else {
-		console.log('Not sure what was created: ', filePath, currentStat);
-	}
+	stage(filePath, currentStat, previousStat);
 }
 
 function handleUpdate(filePath, currentStat, previousStat) {
 	console.log('handleUpdate: ', filePath);
 
-	if (currentStat.isDirectory()) {
-		//dir
-	} else if (currentStat.isFile()) {
-		saveBlob(filePath, currentStat, previousStat);
-	} else {
-		console.log('Not sure what was created: ', filePath, currentStat);
-	}
+	stage(filePath, currentStat, previousStat);
 }
 
 function handleDelete(filePath, currentStat, previousStat) {
 	console.log('handleDelete: ', filePath);
 }
 
+function stage(filePath, currentStat, previousStat) {
+	var t
+	  , name = path.basename(filePath)
+	  , base = path.basename(home)
+	  , ex_home = new RegExp('(' + base + ').*')
+	  , startPath = name
+	  , i = filePath.match(ex_home).index; // index of where home directory starts
+
+	// create the index is it doesn't exist
+	if (!trie.get(base)) index = trie.put(base, []);
+	
+	// remove parts before begining of relative home directory
+	if (i >= 0) startPath = filePath.slice(i-1);
+
+	// split remaining parts
+	startPath = startPath.split(path.sep);
+
+	if (currentStat.isDirectory()) {
+		//stage directory in index file
+		if (!previousStat || (currentStat.mtime.getTime() > previousStat.mtime.getTime())) {
+			startPath = startPath.join('/')
+			// create path object in index if doesn't exist
+			if (!index.get(startPath)) index.put(startPath, []);
+		}
+	} else if (currentStat.isFile()) {
+		// remove filename portion
+		startPath.splice(startPath.length-1);
+		// join to make give relative path without filename
+		startPath = startPath.join('/');
+		if (!previousStat || (currentStat.mtime.getTime() > previousStat.mtime.getTime())) {
+			//stage file within the index sub object associated with the subdirectory
+			t = index.get(startPath);
+			t.push({
+				name: name,
+				mode: 0100644
+			});
+			index.put(startPath, t);
+		}
+	} else {
+		console.log('Not sure what was created: ', filePath, currentStat);
+	}
+}
+
 function saveBlob(filePath, currentStat, previousStat, i) {
 	var name = path.basename(filePath);
-	var base = path.dirname(filePath);
+	var dir = path.dirname(filePath);
 	var ebusy = /EBUSY/ig;
 
 	if (!previousStat || currentStat.size !== previousStat.size) {
@@ -123,7 +146,7 @@ function saveBlob(filePath, currentStat, previousStat, i) {
 				};
 				blob = {
 					name: name,
-					base: base,
+					dir: dir,
 					tree: tree,
 					stat: currentStat
 				};
@@ -140,7 +163,7 @@ function commitChanges(blob) {
 		var node;
 		if (err) throw err;
 //		console.log('Saved tree: ', treeHash);
-		nodes = index[blob.base] || [];
+		nodes = index[blob.dir] || [];
 		console.log(nodes);
 		nodes.push({
 			hash: treeHash,
@@ -148,7 +171,7 @@ function commitChanges(blob) {
 		});
 		var commit = {
 			tree: treeHash,
-			parent: index['__parent'],
+			parent: __parent,
 			author: commiter,
 			commiter: commiter,
 			message: blob.name + ' - ' + blob.stat.mtime
@@ -156,7 +179,7 @@ function commitChanges(blob) {
 		repo.saveAs('commit', commit, function(err, commitHash) {
 			if (err) throw err;
 //			console.log('Saved commit: ', commitHash);
-			index['__parent'] = commitHash;
+			__parent = commitHash;
 			repo.updateHead(commitHash, moveHead);
 		});
 	});
